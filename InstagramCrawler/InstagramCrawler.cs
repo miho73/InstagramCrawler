3fe341c3711a2033bd5f97d1e9eb7bafeaae4f83
miho73 <hyrexfiels@gmail.com>
2020-11-17 02:24:26 +0900

    Dev1

---
+++ InstagramCrawler/InstagramCrawler.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using InstagramCrawling;
+
+namespace InstagramCrawler
+{
+    class InstagramCrawler
+    {
+        public static void Write(string content) {Console.Write(content);}
+        public static void WriteLine(string content) {Console.WriteLine(content);}
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            WriteLine("Welcome to the INSTAGRAM CRWALER");
+            WriteLine("Press any key to continue");
+            Console.ReadKey();
+            WriteLine("=================================");
+            Write("Instagram profile internet address: ");
+            string link = Console.ReadLine();
+            Write("Saving path: ");
+            string path = Console.ReadLine();
+            if(!Directory.Exists(path))
+            {
+                WriteLine("Given directory is not exists. Would you want to make a directory(y/n): ");
+                string c = Console.ReadLine();
+                if (c.ToLower() == "y")
+                {
+                    Directory.CreateDirectory(path);
+                }
+                else
+                {
+                    WriteLine("Canceled");
+                    return;
+                }
+            }
+            string id;
+            StringBuilder pwd = new StringBuilder();
+            Write("Instagram Phone number, username or email: ");
+            id = Console.ReadLine();
+            Write("Password: ");
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter) break;
+                if (key.Key == ConsoleKey.Backspace) pwd.Remove(pwd.Length - 1, 1);
+                pwd.Append(key.KeyChar);
+            }
+            WriteLine("\nPress any key to start");
+            Console.ReadKey();
+            try
+            {
+                Write("Initializing...\n");
+                Crawler crawler = new Crawler();
+                WriteLine("Completed");
+                Write("Logging in...");
+                crawler.Login(id, pwd.ToString(), link);
+                WriteLine("Completed");
+                WriteLine("Getting list of posts...");
+                List<string> url = crawler.GetPhotoUrl();
+                int cnt = 0;
+                foreach (string s in url)
+                {
+                    WriteLine(s);
+                }
+                WriteLine("Completed");
+                WriteLine("Total " + url.Count + "posts.");
+                WriteLine("Getting list of images");
+                List<string> finalLinks = new List<string>();
+                foreach (string s in url)
+                {
+                    WriteLine("CRAWL FROM " + s);
+                    List<string> lx = crawler.GetFrom(s);
+                    foreach (string l in lx)
+                    {
+                        finalLinks.Add(l);
+                    }
+                    WriteLine(s + " Completed");
+                }
+                WriteLine("Completed");
+                WriteLine("Downloading");
+                foreach (string l in finalLinks)
+                {
+                    crawler.SaveAs(l, path, cnt);
+                    WriteLine("Download from " + l);
+                }
+                WriteLine("\nTotal " + cnt + " pictures");
+                WriteLine("Completed");
+            } catch(Exception e)
+            {
+                Console.Error.WriteLine("Error: " + e.Message);
+                Console.Error.Write("Crawling stopped");
+                return;
+            }
+        }
+    }
+}
---
+++ InstagramCrawling/Crawler.cs
@@ -0,0 +1,95 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace InstagramCrawling
+{
+    public class Crawler
+    {
+        public ChromeDriver driver;
+        public ChromeOptions options = new ChromeOptions();
+        public Crawler()
+        {
+            options.AddArgument("headless");
+            driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://www.instagram.com/");
+        }
+        public void Login(string id, string pwd, string loopback)
+        {
+            Thread.Sleep(500);
+            var idField = driver.FindElementByXPath(@"//*[@id='loginForm']/div/div[1]/div/label/input");
+            var pwdField = driver.FindElementByXPath(@"//*[@id='loginForm']/div/div[2]/div/label/input");
+            var submit = driver.FindElementByXPath("//*[@id=\"loginForm\"]/div/div[3]/button");
+            idField.SendKeys(id);
+            pwdField.SendKeys(pwd);
+            submit.Click();
+            Thread.Sleep(4000);
+            driver.Navigate().GoToUrl(loopback);
+        }
+        public List<string> GetPhotoUrl()
+        {
+            List<string> ret = new List<string>();
+            while(true)
+            {
+                try
+                {
+                    ReadOnlyCollection<IWebElement> hrefs = driver.FindElementsByClassName("v1Nh3");
+                    driver.FindElementByClassName("By4nA");
+                    foreach (IWebElement ele in hrefs)
+                    {
+                        string lnk = ele.FindElement(By.TagName("a")).GetAttribute("href");
+                        if (!ret.Contains(lnk))
+                        {
+                            ret.Add(lnk);
+                        }
+                    }
+                    driver.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+                } catch(Exception)
+                {
+                    break;
+                }
+                Thread.Sleep(300);
+            }
+            ReadOnlyCollection<IWebElement> hrefsx = driver.FindElementsByClassName("v1Nh3");
+            foreach (IWebElement ele in hrefsx)
+            {
+                string lnk = ele.FindElement(By.TagName("a")).GetAttribute("href");
+                if (!ret.Contains(lnk))
+                {
+                    ret.Add(lnk);
+                }
+            }
+            return ret;
+        }
+        public List<string> GetFrom(string url)
+        {
+            List<string> ret = new List<string>();
+            driver.Navigate().GoToUrl(url);
+            Thread.Sleep(1000);
+            try
+            {
+                driver.ExecuteScript("document.getElementsByClassName('Z666a')[0].remove();");
+            }
+            catch { }
+            ReadOnlyCollection<IWebElement> eles = driver.FindElementsByClassName("FFVAD");
+            foreach(IWebElement ele in eles)
+            {
+                ret.Add(ele.GetAttribute("src"));
+            }
+            return ret;
+        }
+        public void SaveAs(string url, string path, int cnt)
+        {
+            if (url == null) return;
+            driver.Navigate().GoToUrl(url);
+            Thread.Sleep(50);
+            Screenshot ss = driver.GetScreenshot();
+            ss.SaveAsFile(path + "\\" + cnt + ".png");
+        }
+    }
+}
